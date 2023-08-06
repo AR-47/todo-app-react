aa21e90c6bdb6ab2a86606d75728514ee7e42ad2
Adil Rahman <arahman.99@outlook.com>
2023-08-06 14:20:48 +0100

    Fetched dummy data from server with axios to display todo items

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoCard } from "./TodoCard";
 import { data } from "../data";
+import axios from "axios";
 
 export interface ITodo {
   id: number;
@@ -10,8 +11,37 @@ export interface ITodo {
   creationDate: string;
 }
 
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://adil-todo-app.onrender.com/items"
+    : "http://localhost:5050/items";
+
 export function TodoList(): JSX.Element {
-  const [todos, setTodos] = useState<ITodo[]>(data);
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        console.log(response);
+        console.log("Entered the .then function");
+        setTodos(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, [todos]);
+
+  //   () => {
+  // const fetchTodos = async () => {
+  //   try {
+  //     const response = await axios.get(baseUrl);
+  //     console.log(response);
+  //     console.log("ENTERED TRY BLOCK");
+  //   } catch (error) {
+  //     console.log(error);
+  //     console.log("ENTERED CATCH BLOCK");
+  //   }
+  // };
+  // fetchTodos();}
 
   return (
     <div>
